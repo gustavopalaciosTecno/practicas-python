6692a33807ccab2258a8587b365d2ff7b352ca85
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-02 17:27:24 -0300

    Variables!

--- 00-Introduccion.py
+++ 00-Introduccion.py
@@ -14,5 +14,6 @@ print(type("hola mundo genial")) # me muestra un tipo de datos string(cadena)
 print(type(15)) # me muestra un tipo de dato integer(numérico)
 print(type(15.8)) # me muestra un tipo de dato float(decimal)
 print(type(True)) # me muestra un tipo de dato boolean(booleano)
-print(type(5 + 6j)) ## me muestra un tipo de datos complex(complejo)
+print(type(5 + 6j)) # me muestra un tipo de datos complex(complejo)
+print(type(print("esto es un comentario"))) # devuelve un NonType (no es un tipo de dato)
 
--- 001-Variables.py
+++ 001-Variables.py
@@ -21,11 +21,27 @@ print("este es el valor de: ", my_bool_variable)
 # Algunas funciones del sistema
 print(len(my_cadena_variable)) # cuenta los caracteres
 
-# Variables en una sola línea
+# Variables en una sola línea 
 name, surname, alias, age = "Gustavo", "Palacios Meyer", "PalaTecno", 41
 print("me llamo: ", name, surname, ". mi edad es de: ", age, ". y mi alias es: ",alias)
 # otra forma de imprimir por pantalla
 print(f"mi nombres es: {name} {surname}, mi edad es de unos: {age} años, y mi alias es:  {alias}")
 
+# entrada por pantalla- pide datos y luego imprime los valores
+#name = input("¿Cuál es tu nombre: ?")
+#year_age = input("¿Cuántos años tienes: ?")
+#print(f"tu nombre es: {name} y tu edad es de {year_age} años")
+"""
+al volver a declarar la vairable name, se machaca la variable de arriba, 
+o sea se vuelve a declarar y puede cambiar su valor
+"""
+# no podemos forzar el tipo de dato
+address: str = 'mi direccion postal'
+print(type(address))
+address = 45
+print(address)
+print(type(address))
 
+# Python tiene un tipado débil
+# al declarar una variable, si volvemos a declarar la misma, tomará un nuevo valor
 
