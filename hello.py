3bcae8097715fc14b00ec7dd537c9b0a2b1d2890
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-02 13:28:07 -0300

    comentarios

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 #hacer un hola mundo con Python
 
 print("hola mundo, bienvenidos al lenguajes de programación Python")
+
+#comentario en varias líneas
+"""
+    
+ este es un comentario en varias líneas
+    
+    
+"""
