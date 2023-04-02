eb527df640c508ba533c8fa9948139c19322c0bb
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-02 18:40:37 -0300

    tipos de variables

---
+++ 002-Operadores.py
@@ -0,0 +1,19 @@
+# Operadores más comunes en Python
+
+
+print('esto es una suma: ', 15 + 4)
+print('esto es una resta: ',15 - 4)
+print('esto es una multiplicacion: ', 15 * 4)
+print('esto es una división',15 / 4)
+print('esto es un módulo: ',10 % 3)
+print('esto es una división entera: ',10 // 3)
+print('esto es un exponente: ',5 ** 3)
+
+# Concatenamos strings
+print("hola" + " " + "Python")
+# no podemos concatenar un entero con un string
+# print("hola " + 4) 
+# la solución sería la siguiente:
+#convertimos el número 4 en un string
+print("hola " + str(4))
+
