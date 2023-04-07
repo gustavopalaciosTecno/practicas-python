3fcf7af5f6c1294ba8a9a1f9573ca80858e2b4b8
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-07 01:23:50 -0300

    listas.

---
+++ 004-listas.py
@@ -0,0 +1,23 @@
+# Listas
+
+mi_lista = list()
+my_other_list = []
+
+print(mi_lista)
+print(len(my_other_list))
+
+my_other_list = [41, 1.77, 'Gustavo', 'Palacios Meyer']
+print(my_other_list)
+print(type(mi_lista))
+print(type(my_other_list))
+
+print(
+    """"
+    <ul>
+    <li>frutas</li>
+    <li>verduras</li>
+    <li>carnes</li>
+    <li>lácteos</li>
+    </ul>
+    """
+)
