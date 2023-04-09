80738850dbd1015c3fd4c50eb9bf5a3aabe52eeb
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-08 21:38:25 -0300

    listas.

--- 004-listas.py
+++ 004-listas.py
@@ -6,18 +6,33 @@ my_other_list = []
 print(mi_lista)
 print(len(my_other_list))
 
+
+
 my_other_list = [41, 1.77, 'Gustavo', 'Palacios Meyer']
 print(my_other_list)
 print(type(mi_lista))
 print(type(my_other_list))
 
-print(
-    """"
-    <ul>
-    <li>frutas</li>
-    <li>verduras</li>
-    <li>carnes</li>
-    <li>lácteos</li>
-    </ul>
-    """
-)
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-3])
+print(my_other_list[-4])
+#print(my_other_list[-5]) nos dará error-> list index out of range (lista de índice fuera de rango)
+#print(my_other_list[-5]) nos dará un error->list index out of range
+
+frutas = ["manzanas", "peras", "bananas", "manzanas", "peras", "frutillas", "manzanas"]
+print(frutas.count("manzanas"))
+
+# desempaquetado de valores
+age = 35
+height = 1.77
+name = "Julio"
+surname = "Saldaña"
+mi_otra_lista = [35, 1.77, "Julio", "Saldaña"]
+age, height, name, surname = mi_otra_lista
+print(name, surname, age, height)
+
+# Concatenar dos listas
+nenas = ["Florencia", "Isabella", "Noemí", "Raquel"]
+varones = ["Julio", "Alberto", "Jahziel", "Mauro"]
+print(nenas + varones)
