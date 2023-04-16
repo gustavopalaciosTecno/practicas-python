6eff006e5beaa0a9df7f2d4070034c393a6f4982
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-16 02:43:37 -0300

    condicionales.

--- 008-condicionales.py
+++ 008-condicionales.py
@@ -5,10 +5,40 @@ ejecutar o no.
 
 """
 
+numero1 = int(input("Ingrese un primer valor numérico: "))
+numero2 = int(input("Ingrese un segundo valor numérico: "))
+
+if numero1 > numero2:
+    print(f"el número: {numero1} es mayor que el {numero2}")
+elif numero1 < numero2:
+    print(f"el número: {numero2} es mayor que el {numero1}")
+else:
+    print("los dos son valores iguales !!!")        
+
+my_condition = True
+
+if my_condition:
+    print("se ejecuta la condición")
+    
+print("El programa continua...")    
 
 
+print("##### programa de acceso a la bailanta ######")
 
+edad = int(input("coloca tu edad: "))
+dinero = int(input("coloca tu nivel adquisitivo: "))
+if edad >= 18 and dinero >= 5000:
+    print("Podes ingresar")
+else:
+    print("No podes ingresar a la bailanta")    
 
+  
+print("##########")
+
+my_string = ""
+
+if not my_string: # realizamos la negación del string
+    print("mi cadena no está vacía") 
 
 
 
