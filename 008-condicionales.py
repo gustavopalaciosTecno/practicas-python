38f32357639107921a448f79b5bd4d18c8b36a96
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-16 01:34:44 -0300

    condicionales.

--- 008-condicionales.py
+++ 008-condicionales.py
@@ -1,7 +1,13 @@
 """
-Condicionales.
+Condicionales. Es la manera de establecer flujo de información de 
+nuestro código, es decir decidir si algo de nuestro cópdigo se va a 
+ejecutar o no.
 
 """
+
+
+
+
 
 
 
