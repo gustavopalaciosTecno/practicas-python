d8ade50a5fbd1cd4972fdce32ce8ecfef96bc63e
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-19 01:20:37 -0300

    clases.

---
+++ 011-clases.py
@@ -0,0 +1,37 @@
+"""
+Clases:
+
+"""
+"""
+class Calcular: # la primera letra se escrine con mayúscula
+    def calculo_Auxiliar(self):
+        valor = float(input("Coloca un valor: "))
+        descuento1 = float(input("Coloca primer valor para descontar: "))
+        descuento2 = float(input("Coloca segundo valor para descontar: "))
+        descuento3 = float(input("Coloca tercer valor para descontar: "))
+    
+        suma_descuentos = descuento1 + descuento2 + descuento3
+        resultado = (valor - suma_descuentos)
+        print("el resutlado final de la operación es: ",resultado)
+
+
+objeto1 = Calcular()
+objeto1.calculo_Auxiliar()
+"""
+class Person:
+    def __init__(self, name, surname):
+       self.full_name = f"{name} {surname}"
+       
+    def caminar(self):
+        print(f"{self.full_name} está caminando")   
+       
+        
+my_person = Person("Néstor", "Palacios Meyer")
+print(my_person.full_name)  
+my_person.caminar()
+        
+        
+         
+        
+
+    
