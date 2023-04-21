0e1bb16984b51669bfe91c1827636cbc8f7e61ba
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-20 21:36:31 -0300

    fechas.

---
+++ INTERMEDIO/014-dates.py
@@ -0,0 +1,13 @@
+"""
+Dates: fechas- representa una hora, un día o tiempo.
+"""
+from datetime import datetime
+
+now = datetime.now()
+
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.year)
+print(now.month)
+print(now.second)
