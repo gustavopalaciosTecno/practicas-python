2c3c8831f678d543f4fdbd7bf779ad7f12b3412d
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-05-09 14:12:26 -0300

    resolucón ejercicios

--- INTERMEDIO/02-desafio.py
+++ INTERMEDIO/02-desafio.py
@@ -45,4 +45,30 @@ def Recibe(palabra1, palabra2):
         
 print(Recibe("amorUIO", "roma"))        
 
+# LA SUCESIÓN DE FIBONACCI
+"""
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en cero.
+la serie de Fibonacci se compone por una sucesión de números en
+la que el siguiente siempre es la suma de los dos anteriores
+0,1,1,2,3,5,8,13....
+"""
+def fibonacci():
+    limite = 50
+    previo = 0
+    next = 1
+    
+    for i in range(limite):
+        
+        print(previo)
+        # fib = previo + next
+        # previo = next
+        # next = fib
+        previo, next = next, previo + next      
+
+    
+fibonacci()    
+
+
+
 
